a5579c9728cd5e42c2cc1b9e600e99ae76bb878a
phn210 <57532941+phn210@users.noreply.github.com>
2022-01-05 15:25:32 +0700

    initial frontend commit

--- server/app.py
+++ server/app.py
@@ -1,20 +1,20 @@
-from flask import Flask,request, url_for, redirect, render_template, jsonify
-import numpy as np
-import tensorflow as tf
-from tensorflow import keras
-import pickle as pkl
+from flask import Flask, request, url_for, redirect, render_template, jsonify
+# import numpy as np
+# import tensorflow as tf
+# from tensorflow import keras
+# import pickle as pkl
 
 
-modelFile='traffic.h5'
-testing_file = "test.p"
+# modelFile='traffic.h5'
+# testing_file = "test.p"
 
-with open(testing_file, mode='rb') as f:
-    test = pkl.load(f)
-X_test = test['features']
-y_test = test['labels']
-X_test = np.array(X_test)
-y_test = np.array(y_test)
-model = keras.models.load_model(modelFile)
+# with open(testing_file, mode='rb') as f:
+#     test = pkl.load(f)
+# X_test = test['features']
+# y_test = test['labels']
+# X_test = np.array(X_test)
+# y_test = np.array(y_test)
+# model = keras.models.load_model(modelFile)
 
 app = Flask(__name__)
 
@@ -22,30 +22,35 @@ app = Flask(__name__)
 def home():
     return {"notice": "welcome!!!"}
 
-@app.route('/evaluate')
-def evaluate():
-    results = model.evaluate(X_test, y_test)
-    return {
-            'results_evaluate': results,
-            }
+@app.route('/api', methods=['GET'])
+def testAPI():
+    return {"test" : "okay"}
 
-@app.route('/predict',methods=['POST'])
-def predict():
-    data = request.get_json()
-    print(data)
-    print(data['index'])
-    index = data['index']
-    img_testing = X_test[index]
-    label_testing = y_test[index]
+# @app.route('/evaluate')
+# def evaluate():
+#     results = model.evaluate(X_test, y_test)
+#     return {
+#             'results_evaluate': results,
+#             }
 
-    actual = np.argmax(label_testing)
+# @app.route('/predict',methods=['POST'])
+# def predict():
+#     data = request.get_json()
+#     print(data)
+#     print(data['index'])
+#     index = data['index']
+#     img_testing = X_test[index]
+#     label_testing = y_test[index]
 
-    prediction = np.argmax(model.predict(img_testing))
-    return {
-            'actual':actual,
-            'prediction':prediction,
-            }
+#     actual = np.argmax(label_testing)
+
+#     prediction = np.argmax(model.predict(img_testing))
+#     return {
+#             'actual':actual,
+#             'prediction':prediction,
+#             }
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+    app.run()
